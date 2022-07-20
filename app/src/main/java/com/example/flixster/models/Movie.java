381bebe45f6384c2f70052f3d6e382c61b6ad57c
bogosla <jdestine68@gmail.com>
2022-07-19 18:29:15 -0700

    add more fields in model Movie.java, add parcels deps

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -4,29 +4,48 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Movie {
+    private final int movie_id;
     private final String title;
     private final String overview;
     private final String poster_path;
     private final String backdrop_path;
     private final float vote_average;
+    private final String release_date;
+    private final double popularity;
+    private final Boolean adult;
     private final static String BASE_URL = "https://image.tmdb.org/t/p/w500";
+    private String BASE_URL_MOVIE = "https://api.themoviedb.org/3/movie/$id/videos?api_key=$api_key";
 
 
-    public Movie(String title, String overview, String poster_path, String backdrop_path, float vote_average) {
+    public Movie(String title, String overview, String poster_path, String backdrop_path, float vote_average, int id, String release_date, double popularity, Boolean adult) {
         this.title = title;
         this.overview = overview;
         this.poster_path = poster_path;
         this.backdrop_path = backdrop_path;
         this.vote_average = vote_average;
+        this.movie_id = id;
+        this.release_date = release_date;
+        this.popularity = popularity;
+        this.adult = adult;
+    }
+
+
+    public static Movie fromJson(JSONObject json) throws JSONException {
+        return new Movie(
+                json.getString("title"),
+                json.getString("overview"),
+                json.getString("poster_path"),
+                json.getString("backdrop_path"),
+                (float)(json.getDouble("vote_average")),
+                json.getInt("id"),
+                json.getString("release_date"),
+                json.getDouble("popularity"),
+                json.getBoolean("adult"));
     }
 
     public boolean has_5_star() {
         return ((int) this.vote_average) > 5;
     }
-
-    public static Movie fromJson(JSONObject json) throws JSONException {
-        return new Movie(json.getString("title"), json.getString("overview"), json.getString("poster_path"), json.getString("backdrop_path"), (float)(json.getDouble("vote_average")));
-}
 
     public String getTitle() {
         return title;
@@ -40,10 +59,17 @@ public class Movie {
         return BASE_URL + poster_path;
     }
 
-    public String getBackdrop_path() {
-        return BASE_URL + backdrop_path;
-    }
-    public float getVote_average() {
-        return vote_average;
+    public String getBackdrop_path() { return BASE_URL + backdrop_path; }
+
+    public String getMovie_path(int id, String api_key) { return String.format(BASE_URL_MOVIE, id, api_key); }
+
+    public float getVote_average() { return vote_average; }
+
+    public double getPopularity() { return popularity; }
+
+    public String getRelease_date() { return release_date; }
+
+    public Boolean getAdult() {
+        return adult;
     }
 }
