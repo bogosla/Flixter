f06e7c6ea555a23a0da055da0d566e8f1eef2523
bogosla <jdestine68@gmail.com>
2022-07-29 13:54:59 -0700

    add binding data and refact AdapterMovie

--- app/src/main/java/com/example/flixster/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/MovieAdapter.java
@@ -2,27 +2,30 @@ package com.example.flixster;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.flixster.databinding.MovieItemBinding;
+import com.example.flixster.databinding.MovieItemStarBinding;
 import com.example.flixster.models.Movie;
 
 import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private static final int HAS5STAR = 1;
-    private List<Movie> mMovies;
-    private Context mContext;
+    private static final int HAS_5_STAR = 1;
+    private static final int HAS_NOT_STAR = 0;
+
+    private final List<Movie> mMovies;
+    private final Context mContext;
+
     private OnClickListener mClickListener;
 
     public void setClickListener(OnClickListener clickListener) {
@@ -30,7 +33,7 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     interface OnClickListener {
-        public void onItemClicked(View view, int position, Movie movie);
+        void onItemClicked(View view, int position, Movie movie);
     }
 
     public MovieAdapter(List<Movie> movies, Context ctx) {
@@ -40,8 +43,7 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     
     @Override
     public int getItemViewType(int position) {
-        Movie current = mMovies.get(position);
-        return current.has_5_star() ? HAS5STAR : 0;
+        return (mMovies.get(position)).has_5_star() ? HAS_5_STAR : HAS_NOT_STAR;
     }
 
     @NonNull
@@ -49,9 +51,12 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 
-        if (viewType == HAS5STAR && getOrientation() == 1)
-            return new ViewHolderStar(inflater.inflate(R.layout.movie_item_star, parent, false));
-        return new ViewHolder(inflater.inflate(R.layout.movie_item, parent, false));
+        if (viewType == HAS_5_STAR && getOrientation() == Configuration.ORIENTATION_PORTRAIT) {
+            MovieItemStarBinding bindingStar = DataBindingUtil.inflate(inflater, R.layout.movie_item_star, parent, false);
+            return new ViewHolderStar(bindingStar);
+        }
+        MovieItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.movie_item, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
@@ -66,18 +71,10 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
                 .error(R.mipmap.ic_launcher_round);
 
         // if orientation in portrait and movie got 5 star, display backdrop_image
-        if (movie.has_5_star() && getOrientation() == 1) {
-             Glide.with(mContext).load(movie.getBackdrop_path()).apply(options).into(((ViewHolderStar)holder).imageview2);
-        } else {
-            // else no 5 star, display title, overview and poster_image (still in portrait)
-            // else, display backdrop_image in landscape
-            ((ViewHolder)holder).titleTextview.setText(movie.getTitle());
-            ((ViewHolder)holder).overviewTextview.setText(movie.getOverview());
-            if (getOrientation() == 1)
-                 Glide.with(mContext).load(movie.getPoster_path()).apply(options).into(((ViewHolder)holder).imageview);
-            else
-                Glide.with(mContext).load(movie.getBackdrop_path()).apply(options).into(((ViewHolder)holder).imageview);
-        }
+        if (holder instanceof ViewHolderStar)
+            ((ViewHolderStar)holder).onBind(movie, options);
+        else
+            ((ViewHolder)holder).onBind(movie, options);
     }
 
 
@@ -87,43 +84,43 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     private int getOrientation() {
-        // return 1 if portrait else 0 for landscape
-        int orientation = mContext.getResources().getConfiguration().orientation;
-        return orientation == Configuration.ORIENTATION_PORTRAIT ? 1 : 0;
+        return mContext.getResources().getConfiguration().orientation;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView titleTextview;
-        public TextView overviewTextview;
-        public ImageView imageview;
+        public MovieItemBinding itemBinding;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.titleTextview = itemView.findViewById(R.id.itemTitle);
-            this.imageview = itemView.findViewById(R.id.itemImg);
-            this.overviewTextview = itemView.findViewById(R.id.itemOverview);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mClickListener.onItemClicked(view, getAdapterPosition(), mMovies.get(getAdapterPosition()));
-                }
-            });
+        public ViewHolder(@NonNull MovieItemBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.itemBinding = itemBinding;
+
+            itemBinding.getRoot().setOnClickListener(view -> mClickListener.onItemClicked(view, getAdapterPosition(), mMovies.get(getAdapterPosition())));
+        }
+
+        public void onBind(Movie m, RequestOptions options) {
+            if (getOrientation() == Configuration.ORIENTATION_LANDSCAPE)
+                Glide.with(mContext).load(m.getBackdrop_path()).apply(options).into(itemBinding.itemPoster);
+            else
+                Glide.with(mContext).load(m.getPoster_path()).apply(options).into(itemBinding.itemPoster);
+            itemBinding.itemTitle.setText(m.getTitle());
+            itemBinding.itemOverview.setText(m.getOverview());
+            itemBinding.executePendingBindings();
         }
     }
 
     // Holder for item with more 5 star
     public class ViewHolderStar extends RecyclerView.ViewHolder {
-        public ImageView imageview2;
+        public MovieItemStarBinding itemBinding;
 
-        public ViewHolderStar(@NonNull View itemView) {
-            super(itemView);
-            this.imageview2 = itemView.findViewById(R.id.itemImg2);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mClickListener.onItemClicked(view, getAdapterPosition(), mMovies.get(getAdapterPosition()));
-                }
-            });
+        public ViewHolderStar(@NonNull MovieItemStarBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.itemBinding = itemBinding;
+
+            itemBinding.itemPoster2.setOnClickListener(view -> mClickListener.onItemClicked(view, getAdapterPosition(), mMovies.get(getAdapterPosition())));
+        }
+        public void onBind(Movie m, RequestOptions options) {
+            Glide.with(mContext).load(m.getBackdrop_path()).apply(options).into(itemBinding.itemPoster2);
+            itemBinding.executePendingBindings();
         }
     }
 }
